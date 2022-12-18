63ff07541f7e65b4c43fe604db99871122858038
Hatice Erzincan Sozeri <haticeesozeri@gmail.com>
2022-12-18 14:41:10 -0500

    New Files Added

--- src/test/java/tests/Day07_IFrame.java
+++ src/test/java/tests/Day07_IFrame.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
+import java.sql.Driver;
+
 public class Day07_IFrame extends TestBase {
     //Create a class: IFrame
     //Create a method: iframeTest
@@ -26,13 +28,17 @@ public class Day07_IFrame extends TestBase {
         String header = driver.findElement(By.xpath("//h3")).getText();
         Assert.assertTrue(header.contains("Editor"));
 
-        // Locate the text box
         //NOTE: TextBox is in the iframe, so we have to switch to the iframe
-        driver.switchTo().frame(0);
+        //GETTING IN THE IFRAME
+        driver.switchTo().frame(0);//INDEX OF IFRAME
+        driver.switchTo().frame("mce_0_ifr");//ID OR NAME OF IFRAME. PREFERRED ONE.
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")));// WEBELEMENT OF IFRAME
 
-        //Delete the text in the text box
+        //        Locate the text box
         WebElement box = driver.findElement(By.xpath("//p"));
-        box.clear();
+
+        //        Delete the text in the text box
+        box.clear();//clear() is used to delete the text is the elements
 
         //Type “This text box is inside the iframe”
         box.sendKeys("This text box is inside the iframe");
@@ -48,3 +54,20 @@ public class Day07_IFrame extends TestBase {
     }
 
 }
+/*
+What might be the reason for your test case to fail?
+There may be wait problem. The page may be very slow. Internet maybe not fast enough.
+Put some more wait. We can use explicit wait in selenium(we will learn later)
+Locator might be incorrect.
+Double check locator and fix if needed
+Data issue. Typo in our expected values.
+If there is any documentation issue, we can talk to BA to fix the document and then we can use correct expected values
+Maybe there is an iframe on the page
+We first need to switch to the frame
+Maybe there is an alert on the page
+We first need to handle the alert by accepting or dismissing
+Maybe the page opens in a new tab or window
+We first need to switch the target window
+It may be a real bug that is caused by the latest codes.
+We discuss the issue with the team then raise a bug ticket if needed
+ */
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -20,6 +20,7 @@ public abstract class TestBase {
 
     @After
     public void tearDown(){
-        driver.quit();
+
+        //driver.quit();
     }
 }
