b4f85975370f5a5813434034ed59d462f835a01f
Hatice Erzincan Sozeri <haticeesozeri@gmail.com>
2023-01-04 21:15:31 -0500

    New Test File Added

---
+++ src/test/java/practices/practice04/Q04.java
@@ -0,0 +1,40 @@
+package practices.practice04;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Q04 extends TestBase {
+    /*
+    Given
+        Go to https://testpages.herokuapp.com/
+    When
+        Click on File Downloads
+    And
+        Click on Server Download
+    Then
+        Verify that file is downloaded
+     */
+    @Test
+    public void test(){
+        // Go to https://testpages.herokuapp.com/
+        driver.get("https://testpages.herokuapp.com/");
+
+        // Click on File Downloads
+        driver.findElement(By.id("download")).click();
+
+        // Click on Server Download
+        driver.findElement(By.id("server-download-a")).click();
+
+        // Verify that file is downloaded
+        String homeDirectory = System.getProperty("user.home");
+        String filePath = homeDirectory + "\\Downloads\\textfile.txt";
+        boolean isDownloaded = Files.exists(Paths.get(filePath));
+        Assert.assertTrue(isDownloaded);
+
+    }
+}
